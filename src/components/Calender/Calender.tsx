8c832f1b95a7985246a845cafee4dc199e6f1ea3
Junhyuck Ko <mrbartrns@naver.com>
2022-10-02 16:50:19 +0900

    design: 컴포넌트 디자인

--- src/components/Calender/Calender.tsx
+++ src/components/Calender/Calender.tsx
@@ -12,27 +12,77 @@ const Calender = () => {
   } = useCalender();
 
   return (
-    <div>
-      {formatYearMonth(calender.year, calender.month)}
-      <CalenderController>
-        <button type="button" onClick={setPrevMonthCalender}>
-          이전
-        </button>
-        <button type="button" onClick={setCurrentMonthCalender}>
-          오늘
-        </button>
-        <button type="button" onClick={setNextMonthCalender}>
-          다음
-        </button>
-      </CalenderController>
-      <CalenderInner calender={calender.calender} />
-    </div>
+    <CalenderContainer>
+      <CalenderHeader>
+        <CalenderHeaderTitle>
+          {formatYearMonth(calender.year, calender.month)}
+        </CalenderHeaderTitle>
+        <CalenderController>
+          <CalenderPrevButton type="button" onClick={setPrevMonthCalender} />
+          <CalenderCurrentButton
+            type="button"
+            onClick={setCurrentMonthCalender}
+          />
+          <CalenderNextButton type="button" onClick={setNextMonthCalender} />
+        </CalenderController>
+      </CalenderHeader>
+      <CalenderInner calender={calender.calender} month={calender.month} />
+    </CalenderContainer>
   );
 };
 
 export default Calender;
 
+const CalenderHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 0;
+`;
+
+const CalenderHeaderTitle = styled.div`
+  font-size: 1.5rem;
+  font-weight: 500;
+`;
+
+const CalenderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const CalenderController = styled.div`
   display: flex;
-  justify-content: flex-end;
+  gap: 0.25rem;
+`;
+
+const CalenderButton = styled.button`
+  flex-shrink: 0;
+  padding: 0.25rem 0.5rem;
+  border-radius: 4px;
+  background-color: transparent;
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgba(55, 53, 47, 0.08);
+  }
+`;
+
+const CalenderPrevButton = styled(CalenderButton)`
+  &::after {
+    content: '<';
+  }
+`;
+
+const CalenderCurrentButton = styled(CalenderButton)`
+  &::after {
+    content: '오늘';
+  }
+`;
+
+const CalenderNextButton = styled(CalenderButton)`
+  &::after {
+    content: '>';
+  }
 `;
--- src/components/Calender/CalenderInner.tsx
+++ src/components/Calender/CalenderInner.tsx
@@ -4,30 +4,105 @@ import type { Calender } from '../../hooks/useCalender/useCalender.helper';
 
 interface Props {
   calender: Calender;
+  month: number;
 }
 
-const CalenderInner = ({ calender }: Props) => (
-  <div className="calender">
+const CalenderInner = ({ calender, month }: Props) => (
+  <CalenderMatrix>
     {Object.entries(calender).map(([key, row]) => (
       <CalenderRow key={key}>
-        {row.map((el) => (
-          <CalenderColumn key={`${el.year}${el.month}${el.date}`}>
-            {formatMonthDate(el.month, el.date)}
+        {row.map((el, index) => (
+          <CalenderColumn
+            key={`${el.year}${el.month}${el.date}`}
+            isWeekend={index === 0 || index === 6}
+          >
+            <CalenderBlockTitle>
+              <CalenderDate
+                isCurrentDate={new Date(Date.now()).getDate() === el.date}
+                isCurrentMonth={new Date(Date.now()).getMonth() === el.month}
+                isUserSetMonth={month === el.month}
+              >
+                {el.date === 1 ? formatMonthDate(el.month, el.date) : el.date}
+              </CalenderDate>
+            </CalenderBlockTitle>
           </CalenderColumn>
         ))}
       </CalenderRow>
     ))}
-  </div>
+  </CalenderMatrix>
 );
 
 export default CalenderInner;
 
-const CalenderRow = styled.div`
-  display: flex;
+interface CalenderColumnProps {
+  isWeekend: boolean;
+}
+
+interface CalenderDateProps {
+  isUserSetMonth: boolean;
+  isCurrentMonth: boolean;
+  isCurrentDate: boolean;
+}
+
+const CalenderMatrix = styled.div`
+  display: grid;
+  grid-template-rows: repeat(6, 1fr);
+  grid-auto-rows: 1fr;
+  border-top: 1px solid rgb(233, 233, 231);
+  border-left: 1px solid rgb(233, 233, 231);
+  border-radius: 2px;
 `;
 
-const CalenderColumn = styled.div`
-  border: 1px solid;
-  width: 100px;
+const CalenderRow = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  grid-auto-columns: 1fr;
+  position: relative;
+`;
+
+const CalenderColumn = styled.div<CalenderColumnProps>`
+  position: relative;
   height: 100px;
+  padding: 0.5rem;
+  border-right: 1px solid rgb(233, 233, 231);
+  border-bottom: 1px solid rgb(233, 233, 231);
+  background-color: ${({ isWeekend }) =>
+    isWeekend ? 'rgb(251, 251, 250)' : '#ffffff'};
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgba(233, 233, 231, 0.3);
+  }
+`;
+
+const CalenderBlockTitle = styled.div`
+  display: flex;
+  // TODO: 나중에 지울 것
+  justify-content: flex-end;
+  padding-bottom: 0.5rem;
+`;
+
+const CalenderDate = styled.span<CalenderDateProps>`
+  display: inline-block;
+  min-width: 22px;
+  min-height: 22px;
+  border-radius: 50%;
+
+  color: ${({ isCurrentMonth, isCurrentDate, isUserSetMonth }) => {
+    if (isCurrentMonth && isCurrentDate) {
+      return `#ffffff`;
+    }
+
+    if (!isUserSetMonth) {
+      return `rgba(55, 53, 47, 0.5)`;
+    }
+
+    return ``;
+  }};
+
+  background-color: ${({ isCurrentMonth, isCurrentDate }) =>
+    isCurrentMonth && isCurrentDate ? `rgb(235, 87, 87)` : ''};
+
+  font-size: 14px;
 `;
