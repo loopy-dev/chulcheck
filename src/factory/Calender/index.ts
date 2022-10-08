9c115b8f8b7e9428198ad95d8d5dbe079b5138e2
Junhyuck Ko <mrbartrns@naver.com>
2022-10-08 22:44:24 +0900

    chore: 필요 없는 타입 제거

--- src/factory/Calender/index.ts
+++ src/factory/Calender/index.ts
@@ -1,7 +1,5 @@
 import Calender from './Caldender';
 import Day from './Day';
 import Week from './Week';
-import type { ReturnType } from './Caldender';
 
-export type { ReturnType };
 export { Calender, Day, Week };
