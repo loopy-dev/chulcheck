05412101df969e6af8a472e8d7eb0c9f68591a92
Junhyuck Ko <mrbartrns@naver.com>
2022-10-12 14:21:52 +0900

    feat: 셀 클릭시 데이터를 추가하기 위한 이벤트 및 메서드 구현
    feature: 클릭 시 출석 배열에 아이템이 추가되도록 구현
    
    refactor: 리팩토링
    
    fix: typo

--- src/api/attendance.ts
+++ src/api/attendance.ts
@@ -1,58 +1,58 @@
-export interface Attendence {
+export interface Attendance {
   id: string;
   user: string;
-  timeStamp: string;
+  timestamp: string;
 }
 
-export const mockData: Attendence[] = [
+export const mockData: Attendance[] = [
   {
     id: '1',
     user: '1',
-    timeStamp: '2022-10-01',
+    timestamp: '2022-10-01',
   },
   {
     id: '2',
     user: '1',
-    timeStamp: '2022-10-02',
+    timestamp: '2022-10-02',
   },
   {
     id: '3',
     user: '1',
-    timeStamp: '2022-10-03',
+    timestamp: '2022-10-03',
   },
   {
     id: '4',
     user: '1',
-    timeStamp: '2022-10-04',
+    timestamp: '2022-10-04',
   },
   {
     id: '5',
     user: '1',
-    timeStamp: '2022-10-05',
+    timestamp: '2022-10-05',
   },
   {
     id: '6',
     user: '1',
-    timeStamp: '2022-10-06',
+    timestamp: '2022-10-06',
   },
   {
     id: '7',
     user: '1',
-    timeStamp: '2022-10-07',
+    timestamp: '2022-10-07',
   },
   {
     id: '8',
     user: '1',
-    timeStamp: '2022-10-08',
+    timestamp: '2022-10-08',
   },
   {
     id: '9',
     user: '1',
-    timeStamp: '2022-10-09',
+    timestamp: '2022-10-09',
   },
   {
     id: '10',
     user: '1',
-    timeStamp: '2022-10-10',
+    timestamp: '2022-10-10',
   },
 ];
--- src/components/Calender/Calender.tsx
+++ src/components/Calender/Calender.tsx
@@ -1,22 +1,46 @@
+import { useCallback } from 'react';
 import styled from 'styled-components';
+import { v4 } from 'uuid';
 import useCalender from '../../hooks/useCalender';
 import useCalenderData from '../../hooks/useCalenderData';
 import { formatYearMonth } from '../../utils/dateFormat';
 import CalenderHeader from './CalenderHeader';
 import CalenderInner from './CalenderInner';
+import type { Attendance } from '../../api/attendance';
 
-interface Props {
-  onClickItem?: React.MouseEventHandler<HTMLDivElement>;
-}
+export type CellClickEventHandler = (timestamp: string) => void;
 
-const Calender = ({ onClickItem }: Props) => {
+const Calender = () => {
   const {
     calender,
     setPrevMonthCalender,
     setCurrentMonthCalender,
     setNextMonthCalender,
   } = useCalender();
-  const items = useCalenderData();
+  const { attendence, addAttendence } = useCalenderData();
+
+  /**
+   * @description
+   * 클릭 시 출석 체크를 진행한다.
+   * ANCHOR
+   * 현재는 임시로 구현된 부분이므로, 추후 변경되어야 함
+   * try...catch block을 이용하여 정시에 출석체크 했는지 체크
+   * 이미 출석했다면, 눌러도 반응이 없도록 해야 함
+   * 자료 구조를 set으로 바꿔야 할 필요성도 있음
+   */
+  const handleClickItem: CellClickEventHandler = useCallback(
+    (timestamp: string) => {
+      // create Attendence Object
+      const newAttendence: Attendance = {
+        id: v4(),
+        user: '1',
+        timestamp,
+      };
+
+      addAttendence(newAttendence);
+    },
+    [addAttendence],
+  );
 
   return (
     <Container>
@@ -28,8 +52,8 @@ const Calender = ({ onClickItem }: Props) => {
       />
       <CalenderInner
         calender={calender}
-        items={items}
-        onClickItem={onClickItem}
+        items={attendence}
+        onClickItem={handleClickItem}
       />
     </Container>
   );
