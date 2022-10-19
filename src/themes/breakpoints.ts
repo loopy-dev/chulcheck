d764f8f202ba820ecbcf47d7337505f5970ee082
Junhyuck Ko <mrbartrns@naver.com>
2022-10-19 20:50:58 +0900

    feat: GlobalLayout 구성

---
+++ src/themes/breakpoints.ts
@@ -0,0 +1,10 @@
+import type { Size } from './fontSizes';
+
+const DEFAULT_BREAKPOINTS: Record<Size, number> = {
+  sm: 576,
+  md: 768,
+  lg: 1200,
+  xl: 1440,
+};
+
+export default DEFAULT_BREAKPOINTS;
