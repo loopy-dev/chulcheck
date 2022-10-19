790022ce3d98f56577651eb865fbf3ceba20ac02
Junhyuck Ko <mrbartrns@naver.com>
2022-10-19 21:58:20 +0900

    refactor: Router 컴포넌트 분리

---
+++ src/routes/router.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
+import { Calender } from '../components/Calender';
+import SigninPage from '../pages/SigninPage';
+import SignupPage from '../pages/SignupPage';
+
+const Router = () => (
+  <Routes>
+    <Route element={<Calender />} path="/" />
+    <Route element={<SigninPage />} path="/signin" />
+    <Route element={<SignupPage />} path="/signup" />
+  </Routes>
+);
+
+export default Router;
