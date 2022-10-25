4f50355abd5a904f8041b7fd82550c949db37f95
Junhyuck Ko <mrbartrns@naver.com>
2022-10-25 15:05:07 +0900

    feat: NotFoundPage 구현

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { H2 } from '../components/shared/Headings';
+
+const NotFoundPage = () => <Title>404 Not Found</Title>;
+
+export default NotFoundPage;
+
+const Title = styled(H2)`
+  text-align: center;
+`;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { Calender } from '../components/Calender';
+import NotFoundPage from '../pages/NotFoundPage';
 import SigninPage from '../pages/SigninPage';
 import SignupPage from '../pages/SignupPage';
 
 const Router = () => (
   <Routes>
+    <Route element={<NotFoundPage />} path="*" />
     <Route element={<Calender />} path="/" />
     <Route element={<SigninPage />} path="/signin" />
     <Route element={<SignupPage />} path="/signup" />
