1ab97f3a7e3529defdf8267f11fd109ed450a4fa
Junhyuck Ko <mrbartrns@naver.com>
2022-11-09 15:30:41 +0900

    feat: formatTime 유틸 함수 구현

--- src/__tests__/dateFormat.test.ts
+++ src/__tests__/dateFormat.test.ts
@@ -1,4 +1,4 @@
-import { formatDate } from '../utils/dateFormat';
+import { formatDate, formatTime } from '../utils/dateFormat';
 
 describe('date format', () => {
   it('format date, options: { format: "YYYY-MM-DD", delimiter: "-" }', () => {
@@ -17,5 +17,14 @@ describe('date format', () => {
     expect(
       formatDate(new Date(2022, 9, 9), { format: 'MM-DD', delimiter: ' ' })
     ).toBe('10 09');
+  });
+
+  it('format time, options: { format: "HH:MM:SS", delimiter: ":" }', () => {
+    expect(
+      formatTime(new Date(1667975238660), {
+        format: 'HH:MM:SS',
+        delimiter: ':',
+      })
+    ).toBe('15:27:18');
   });
 });
