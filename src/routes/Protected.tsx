ffec9148676263eaa5d1550dc43f7211d5de615e
Junhyuck Ko <mrbartrns@naver.com>
2022-12-01 10:07:14 +0900

    feat: Protected Route 구현

---
+++ src/routes/Protected.tsx
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Storage from '../utils/storage';
+
+const storage = new Storage();
+
+interface Props {
+  children: JSX.Element;
+}
+
+/**
+ * @description
+ * PrivateRoute는 로그인 여부를 판단하여, 로그인하지 않았다면 login page로 이동,
+ * 로그인했다면 기존 페이지로 이동합니다.
+ */
+const Protected = ({ children }: Props) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const value = storage.getItem<string>('token', '');
+
+    if (!value) {
+      navigate('/signin');
+    }
+  }, [navigate]);
+
+  return children;
+};
+
+export default Protected;
