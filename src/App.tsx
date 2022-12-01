f5fe8fd836be264c6776e7a526cd23f705bb9905
Junhyuck Ko <mrbartrns@naver.com>
2022-12-01 11:50:42 +0900

    feat: Header 위치를 메인페이지로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css';
-import Header from './components/Header';
 import GlobalLayout from './components/layouts/GlobalLayout';
 import Router from './routes';
 
 const App = () => (
-  <>
-    <Header />
-    <GlobalLayout>
-      <Router />
-    </GlobalLayout>
-  </>
+  <GlobalLayout>
+    <Router />
+  </GlobalLayout>
 );
 
 export default App;
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,5 @@
 import { Calender } from '../components/Calender';
+import Header from '../components/Header';
 import AttendanceCard from '../components/MainPage';
 import MainPageLayout from '../components/layouts/MainPageLayout';
 import AttendanceProvider from '../contexts/AttendanceProvider';
@@ -6,6 +7,7 @@ import AttendanceProvider from '../contexts/AttendanceProvider';
 const MainPage = () => {
   return (
     <AttendanceProvider>
+      <Header />
       <MainPageLayout left={<Calender />} right={<AttendanceCard />} />
     </AttendanceProvider>
   );
