74f8a134b2aaa86c98b13b5e33d4a8318f8ea3ab
Junhyuck Ko <mrbartrns@naver.com>
2022-12-02 14:36:17 +0900

    feat: Select Component 구현

---
+++ src/components/shared/Select/ArrowDown.tsx
@@ -0,0 +1,18 @@
+// TODO - 위치 옮기기
+const ArrowDown = () => {
+  return (
+    <span>
+      <svg
+        fill="currentColor"
+        height="10px"
+        viewBox="0 0 9 6"
+        width="10px"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M4.5 6L8.39711 0.75H0.602886L4.5 6Z" fill="currentColor" />
+      </svg>
+    </span>
+  );
+};
+
+export default ArrowDown;
